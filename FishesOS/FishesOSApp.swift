656be9e9f5220eee573dbd4731351cef2e459504
kejlor <woj450@gmail.com>
2022-02-03 20:43:55 +0100

    Initial Commit

---
+++ FishesOS/FishesOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishesOSApp.swift
+//  FishesOS
+//
+//  Created by Bartosz Wojtkowiak on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FishesOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
