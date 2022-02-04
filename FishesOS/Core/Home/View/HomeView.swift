5faa0b379c123060039d5d1c1109e2c6bf79603e
kejlor <woj450@gmail.com>
2022-02-04 15:15:49 +0100

    Main view of the application

---
+++ FishesOS/Core/Home/View/HomeView.swift
@@ -0,0 +1,57 @@
+//
+//  HomeView.swift
+//  FishesOS
+//
+//  Created by Bartosz Wojtkowiak on 03/02/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @EnvironmentObject private var vm: HomeViewModel
+    @State private var showFishDetail = false
+    @State private var selectedFish: FishModel? = nil
+    
+    var body: some View {
+        VStack {
+            allFishes
+        }
+        .background(
+            NavigationLink(
+                destination: FishDetailView(fish: $selectedFish),
+                isActive: $showFishDetail,
+                label: { EmptyView() })
+        )
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+        .environmentObject(dev.homeVM)
+    }
+}
+
+extension HomeView {
+    private var allFishes: some View {
+        List {
+            ForEach(vm.allFishes) { fish in
+                Text(fish.speciesName)
+                    .onTapGesture {
+                        segue(fish: fish)
+                    }
+            }
+
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private func segue(fish: FishModel) {
+        selectedFish = fish
+        showFishDetail.toggle()
+    }
+}
