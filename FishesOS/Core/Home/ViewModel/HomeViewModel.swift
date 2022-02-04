7e817c76d4a75f94bbbd43b42ce7abd8565aa01f
kejlor <woj450@gmail.com>
2022-02-04 19:08:22 +0100

    Added functions to set sort by alphabetic order, filter and sort fishes array and to search array by user input

--- FishesOS/Core/Home/ViewModel/HomeViewModel.swift
+++ FishesOS/Core/Home/ViewModel/HomeViewModel.swift
@@ -11,19 +11,53 @@ import SwiftUI
 
 class HomeViewModel: ObservableObject {
     @Published var allFishes: [Fish] = []
+    @Published var searchText = ""
+    @Published var sortOption: SortOption = .alfabetic
     
     private var fishesDataService = FishesDataService()
     private var anyCancellables = Set<AnyCancellable>()
+    
+    enum SortOption {
+        case alfabetic
+    }
     
     init() {
         addSubscriber()
     }
     
     func addSubscriber() {
-        fishesDataService.$allFishes
-            .sink { [weak self] (returnedFishes) in
-                self?.allFishes = returnedFishes
+        $searchText
+            .combineLatest(fishesDataService.$allFishes, $sortOption)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterAndSortFishes)
+            .sink { [weak self] (returnedFish) in
+                self?.allFishes = returnedFish
             }
             .store(in: &anyCancellables)
+    }
+    
+    private func filterAndSortFishes(text: String, fishes: [Fish], sort: SortOption) -> [Fish] {
+        var updatedFishes = filterFishes(text: text, fishes: fishes)
+        sortFishes(sort: sort, fishes: &updatedFishes)
+        return updatedFishes
+    }
+    
+    private func filterFishes(text: String, fishes: [Fish]) -> [Fish] {
+        guard !text.isEmpty else {
+            return fishes
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return fishes.filter { (fish) -> Bool in
+            return fish.speciesName.lowercased().contains(lowercasedText)
+        }
+    }
+    
+    private func sortFishes(sort: SortOption, fishes: inout [Fish]) {
+        switch sort {
+        case .alfabetic:
+            fishes.sort(by: { $0.speciesName < $1.speciesName })
+        }
     }
 }
