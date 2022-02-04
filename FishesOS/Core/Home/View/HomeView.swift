6803e7b833bb760155a60005fa4b4cb19f2a42a4
kejlor <woj450@gmail.com>
2022-02-04 19:06:12 +0100

    Added search bar on top of the view to filter list by user input

--- FishesOS/Core/Home/View/HomeView.swift
+++ FishesOS/Core/Home/View/HomeView.swift
@@ -14,6 +14,7 @@ struct HomeView: View {
     
     var body: some View {
         VStack {
+            SearchBarView(searchText: $vm.searchText)
             allFishes
         }
         .background(
